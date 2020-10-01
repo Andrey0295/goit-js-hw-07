ceccf51530ea30cf72d5e841d8f615bbb4edc345
Andrey0295 <andreevicandrej00@gmail.com>
2020-10-01 21:14:53 +0300

    Работает над task-3

---
+++ js/task-3.js
@@ -0,0 +1,51 @@
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const getListEl = document.querySelector('#gallery');
+getListEl.style.display = 'flex';
+getListEl.style.flexWrap = 'wrap';
+getListEl.style.listStyle = 'none';
+
+const makeItemGallery = images.map(function (image) {
+  const imageUrl = image.url;
+  const imageDescription = image.alt;
+  return `<li><img src="${imageUrl}" alt="${imageDescription}" class="item-gallery"></li>`;
+});
+
+getListEl.insertAdjacentHTML('afterbegin', makeItemGallery);
+
+console.log(getListEl);
+
+const getImage = document.querySelectorAll('.item-gallery');
+console.log(getImage);
+
+// const elements = colorPickerOptions.map(option => {
+//   const buttonEl = document.createElement('button');
+//   buttonEl.type = 'button';
+//   buttonEl.classList.add('color-picker__option');
+//   buttonEl.textContent = option.label;
+//   buttonEl.style.backgroundColor = option.color;
+
+//   return buttonEl;
+// });
+
+// Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css - классы.
